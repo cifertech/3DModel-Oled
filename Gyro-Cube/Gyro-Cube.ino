73c0e81024f526c61413abe653927eea24172ce4
cifertech <62047147+cifertech@users.noreply.github.com>
2021-03-20 02:19:17 +0330

    Add files via upload

---
+++ Gyro-Cube/Gyro-Cube.ino
@@ -0,0 +1,338 @@
+#include <SPI.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <Wire.h>
+
+const int MPU=0x68;  
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define OLED_address  0x3c 
+
+float xx,xy,xz;
+float yx,yy,yz;
+float zx,zy,zz;
+
+float fact;
+
+int Xan,Yan;
+
+int Xoff;
+int Yoff;
+int Zoff;
+
+struct Point3d
+{
+  int x;
+  int y;
+  int z;
+};
+
+struct Point2d
+{
+  int x;
+  int y;
+};
+
+
+
+int LinestoRender; 
+int OldLinestoRender; 
+
+struct Line3d
+{
+  Point3d p0;
+  Point3d p1;
+};
+
+struct Line2d
+{
+  Point2d p0;
+  Point2d p1;
+};
+
+Line3d Lines[12];  //تعداد خطوط قابل پردازش
+Line2d Render[12];
+Line2d ORender[12];
+
+
+void SetVars(void)
+{
+  float Xan2,Yan2,Zan2;
+  float s1,s2,s3,c1,c2,c3;
+  
+  Xan2 = Xan / fact; // تبدیل مقادیر زاویه
+  Yan2 = Yan / fact;
+  
+    
+  s1 = sin(Yan2);
+  s2 = sin(Xan2);
+  
+  c1 = cos(Yan2);
+  c2 = cos(Xan2);
+
+  xx = c1;
+  xy = 0; 
+  xz = -s1;
+
+  yx = (s1 * s2);
+  yy = c2;
+  yz = (c1 * s2);
+
+  zx = (s1 * c2);
+  zy = -s2;
+  zz = (c1 * c2);
+}
+
+
+void ProcessLine(struct Line2d *ret,struct Line3d vec)
+{
+  float zvt1;
+  int xv1,yv1,zv1;
+
+  float zvt2;
+  int xv2,yv2,zv2;
+  
+  int rx1,ry1;
+  int rx2,ry2;
+ 
+  int x1;
+  int y1;
+  int z1;
+
+  int x2;
+  int y2;
+  int z2;
+  
+  int Ok;
+  
+  x1=vec.p0.x;
+  y1=vec.p0.y;
+  z1=vec.p0.z;
+
+  x2=vec.p1.x;
+  y2=vec.p1.y;
+  z2=vec.p1.z;
+
+  Ok=0; // بطور پیشفرض 0
+
+  xv1 = (x1 * xx) + (y1 * xy) + (z1 * xz);
+  yv1 = (x1 * yx) + (y1 * yy) + (z1 * yz);
+  zv1 = (x1 * zx) + (y1 * zy) + (z1 * zz);
+
+  zvt1 = zv1 - Zoff;
+
+
+  if( zvt1 < -5){
+    rx1 = 256 * (xv1 / zvt1) + Xoff;
+    ry1 = 256 * (yv1 / zvt1) + Yoff;
+    Ok=1; // ok we are alright for point 1.
+  }
+  
+  
+  xv2 = (x2 * xx) + (y2 * xy) + (z2 * xz);
+  yv2 = (x2 * yx) + (y2 * yy) + (z2 * yz);
+  zv2 = (x2 * zx) + (y2 * zy) + (z2 * zz);
+
+  zvt2 = zv2 - Zoff;
+
+
+  if( zvt2 < -5){
+    rx2 = 256 * (xv2 / zvt2) + Xoff;
+    ry2 = 256 * (yv2 / zvt2) + Yoff;
+  } else
+  {
+    Ok=0;
+  }
+  
+  if(Ok==1){
+  ret->p0.x=rx1;
+  ret->p0.y=ry1;
+
+  ret->p1.x=rx2;
+  ret->p1.y=ry2;
+  }
+}
+
+
+void setup() {
+ Wire.begin();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3c);  
+  display.clearDisplay();   // پاک کردن صفحه 
+
+
+Wire.begin();
+
+  fact = 180 / 3.14159265358979323846264338327950; // برای تبدیل زاویه به رادیان
+  
+  Xoff = 60; // محل قرار گیری مکعب در محور x
+  Yoff = 15; //  محل قرار گیری مکعب در محور y
+  Zoff = 1300;   //اندازه مکعب
+
+
+// Front Face.
+
+  Lines[0].p0.x=-50;
+  Lines[0].p0.y=-50;
+  Lines[0].p0.z=50;
+  Lines[0].p1.x=50;
+  Lines[0].p1.y=-50;
+  Lines[0].p1.z=50;
+
+  Lines[1].p0.x=50;
+  Lines[1].p0.y=-50;
+  Lines[1].p0.z=50;
+  Lines[1].p1.x=50;
+  Lines[1].p1.y=50;
+  Lines[1].p1.z=50;
+
+  Lines[2].p0.x=50;
+  Lines[2].p0.y=50;
+  Lines[2].p0.z=50;
+  Lines[2].p1.x=-50;
+  Lines[2].p1.y=50;
+  Lines[2].p1.z=50;
+
+  Lines[3].p0.x=-50;
+  Lines[3].p0.y=50;
+  Lines[3].p0.z=50;
+  Lines[3].p1.x=-50;
+  Lines[3].p1.y=-50;
+  Lines[3].p1.z=50;
+
+
+//back face.
+
+  Lines[4].p0.x=-50;
+  Lines[4].p0.y=-50;
+  Lines[4].p0.z=-50;
+  Lines[4].p1.x=50;
+  Lines[4].p1.y=-50;
+  Lines[4].p1.z=-50;
+
+  Lines[5].p0.x=50;
+  Lines[5].p0.y=-50;
+  Lines[5].p0.z=-50;
+  Lines[5].p1.x=50;
+  Lines[5].p1.y=50;
+  Lines[5].p1.z=-50;
+
+  Lines[6].p0.x=50;
+  Lines[6].p0.y=50;
+  Lines[6].p0.z=-50;
+  Lines[6].p1.x=-50;
+  Lines[6].p1.y=50;
+  Lines[6].p1.z=-50;
+
+  Lines[7].p0.x=-50;
+  Lines[7].p0.y=50;
+  Lines[7].p0.z=-50;
+  Lines[7].p1.x=-50;
+  Lines[7].p1.y=-50;
+  Lines[7].p1.z=-50;
+
+// چهار گوشه مکعب.
+
+  Lines[8].p0.x=-50;
+  Lines[8].p0.y=-50;
+  Lines[8].p0.z=50;
+  Lines[8].p1.x=-50;
+  Lines[8].p1.y=-50;
+  Lines[8].p1.z=-50;
+
+  Lines[9].p0.x=50;
+  Lines[9].p0.y=-50;
+  Lines[9].p0.z=50;
+  Lines[9].p1.x=50;
+  Lines[9].p1.y=-50;
+  Lines[9].p1.z=-50;
+
+  Lines[10].p0.x=-50;
+  Lines[10].p0.y=50;
+  Lines[10].p0.z=50;
+  Lines[10].p1.x=-50;
+  Lines[10].p1.y=50;
+  Lines[10].p1.z=-50;
+
+  Lines[11].p0.x=50;
+  Lines[11].p0.y=50;
+  Lines[11].p0.z=50;
+  Lines[11].p1.x=50;
+  Lines[11].p1.y=50;
+  Lines[11].p1.z=-50;
+
+  LinestoRender=12;
+  OldLinestoRender=LinestoRender;
+ 
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);  
+  Wire.write(0);     // 0 برای بیدار کردن ماژول
+  Wire.endTransmission(true);
+  
+}
+/***********************************************************************************************************************************/
+void RenderImage( void)
+{
+
+ for (int i=0; i<OldLinestoRender; i++ )
+  {
+   display.drawLine(ORender[i].p0.x,ORender[i].p0.y,ORender[i].p1.x,ORender[i].p1.y, BLACK); // خطوط قدیمی
+  }
+
+    
+  for (int i=0; i<LinestoRender; i++ )
+  {
+   display.drawLine(Render[i].p0.x,Render[i].p0.y,Render[i].p1.x,Render[i].p1.y, WHITE);
+  }
+  OldLinestoRender=LinestoRender;
+  
+  
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); 
+  Wire.endTransmission(true);
+  Wire.requestFrom(MPU,14,true);  
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  
+  //delay(10);
+}
+
+void loop() {
+  display.display();
+  display.clearDisplay();   
+ 
+  //برای چرخش مکعب
+  int xOut=0;
+  int yOut=0;
+  
+  xOut = map(AcX,-17000,17000,-50,50);
+    yOut = map(AcY,-17000,17000,-50,50);
+  
+ Xan+=xOut;
+ Yan+=yOut;
+  
+
+  Yan=Yan % 360;
+  Xan=Xan % 360; 
+  
+
+
+  SetVars(); 
+  for(int i=0; i<LinestoRender ; i++)
+  {
+    ORender[i]=Render[i]; 
+    ProcessLine(&Render[i],Lines[i]); 
+  }  
+  
+  RenderImage(); 
+  
+  
+ 
+}
